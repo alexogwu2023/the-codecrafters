c36692b1686e38c6b5cea9c6dbf68e8380e9b5d8
alexogwu2023 <alexogwu2023@gmail.com>
2026-04-01 16:12:36 +0100

    dmh

--- sentinel-reborn/main.go
+++ sentinel-reborn/main.go
@@ -2,22 +2,50 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strings"
 )
 
 func LowerCase(text string) string {
 	words := strings.Fields(text)
 	for i, s := range words {
-		if s == "(low)" {
-			words[i-1] = strings.ToLower(words[i-1])
-			words = append(words[:i], words[i+1:]...)
+		if s == "(low," {
+			n := int(words[i+1][0] - '0')
+
+			for j := i - n; j < i; j++ {
+				if j >= 0 {
+					words[j] = strings.ToLower(words[j])
+				}
+			}
+			words = append(words[:i], words[i+2:]...)
+			break
+			
 		}
 
-		//words[i] = strings.ToLower(words[i])
 	}
 	return strings.Join(words, " ")
 
 }
 func main() {
-	fmt.Println(LowerCase("hope IT WAS THE FIRST (low, 2) come"))
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: go run main.go input.txt output.txt")
+		return
+	}
+
+	inputfile := os.Args[1]
+	outputfile := os.Args[2]
+
+	data, err := os.ReadFile(inputfile)
+	if err != nil {
+		fmt.Println("error reading file:", err)
+		return
+	}
+
+	result := LowerCase(string(data))
+
+	err = os.WriteFile(outputfile, []byte(result), 0644)
+	if err != nil {
+		fmt.Println("error writing file:", err)
+		return
+	}
 }
