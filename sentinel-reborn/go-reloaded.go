4abf2bca782b20b9840bb5c64bd0bf68a5d9b0f2
alexogwu2023 <alexogwu2023@gmail.com>
2026-04-07 11:17:00 +0100

    hf

--- sentinel-reborn/go-reloaded.go
+++ sentinel-reborn/go-reloaded.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	//"fmt"
+	//"os"
 	"strconv"
 	"strings"
 	"unicode"
 )
-
 
 // DONE
 func converter(input string) string {
@@ -32,119 +33,147 @@ func converter(input string) string {
 }
 
 func capitalizeWord(word string) string {
-    runes := []rune(strings.ToLower(word))
-    if len(runes) > 0 {
-        runes[0] = unicode.ToUpper(runes[0])
-    }
-    return string(runes)
+	runes := []rune(strings.ToLower(word))
+	if len(runes) > 0 {
+		runes[0] = unicode.ToUpper(runes[0])
+	}
+	return string(runes)
 }
 
 func toLowerCase(text string) string {
-    words := strings.Fields(text)
+	words := strings.Fields(text)
 
-    for i := 0; i < len(words); i++ {
-        if words[i] == "(low)" {
-            if i-1 >= 0 {
-                words[i-1] = strings.ToLower(words[i-1])
-            }
-            words = append(words[:i], words[i+1:]...)
-            i--
-            continue
-        }
+	for i := 0; i < len(words); i++ {
+		if words[i] == "(low)" {
+			if i-1 >= 0 {
+				words[i-1] = strings.ToLower(words[i-1])
+			}
+			words = append(words[:i], words[i+1:]...)
+			i--
+			continue
+		}
 
-        if words[i] == "(low," && i+1 < len(words) {
-            countStr := strings.TrimSuffix(words[i+1], ")")
-            n, err := strconv.Atoi(countStr)
-            if err == nil {
-                start := i - n
-                if start < 0 {
-                    start = 0
-                }
-                for j := start; j < i; j++ {
-                    words[j] = strings.ToLower(words[j])
-                }
-            }
-            words = append(words[:i], words[i+2:]...)
-            i--
-        }
-    }
+		if words[i] == "(low," && i+1 < len(words) {
+			countStr := strings.TrimSuffix(words[i+1], ")")
+			n, err := strconv.Atoi(countStr)
+			if err == nil {
+				start := i - n
+				if start < 0 {
+					start = 0
+				}
+				for j := start; j < i; j++ {
+					words[j] = strings.ToLower(words[j])
+				}
+			}
+			words = append(words[:i], words[i+2:]...)
+			i--
+		}
+	}
 
-    return strings.Join(words, " ")
+	return strings.Join(words, " ")
 }
 
 func toUpperCase(text string) string {
-    words := strings.Fields(text)
+	words := strings.Fields(text)
 
-    for i := 0; i < len(words); i++ {
-        if words[i] == "(up)" {
-            if i-1 >= 0 {
-                words[i-1] = strings.ToUpper(words[i-1])
-            }
-            words = append(words[:i], words[i+1:]...)
-            i--
-            continue
-        }
+	for i := 0; i < len(words); i++ {
+		if words[i] == "(up)" {
+			if i-1 >= 0 {
+				words[i-1] = strings.ToUpper(words[i-1])
+			}
+			words = append(words[:i], words[i+1:]...)
+			i--
+			continue
+		}
 
-        if words[i] == "(up," && i+1 < len(words) {
-            countStr := strings.TrimSuffix(words[i+1], ")")
-            n, err := strconv.Atoi(countStr)
-            if err == nil {
-                start := i - n
-                if start < 0 {
-                    start = 0
-                }
-                for j := start; j < i; j++ {
-                    words[j] = strings.ToUpper(words[j])
-                }
-            }
-            words = append(words[:i], words[i+2:]...)
-            i--
-        }
-    }
+		if words[i] == "(up," && i+1 < len(words) {
+			countStr := strings.TrimSuffix(words[i+1], ")")
+			n, err := strconv.Atoi(countStr)
+			if err == nil {
+				start := i - n
+				if start < 0 {
+					start = 0
+				}
+				for j := start; j < i; j++ {
+					words[j] = strings.ToUpper(words[j])
+				}
+			}
+			words = append(words[:i], words[i+2:]...)
+			i--
+		}
+	}
 
-    return strings.Join(words, " ")
+	return strings.Join(words, " ")
 }
 
 func toTitleCase(text string) string {
-    words := strings.Fields(text)
+	words := strings.Fields(text)
 
-    for i := 0; i < len(words); i++ {
-        if words[i] == "(cap)" {
-            if i-1 >= 0 {
-                words[i-1] = capitalizeWord(words[i-1])
-            }
-            words = append(words[:i], words[i+1:]...)
-            i--
-            continue
-        }
+	for i := 0; i < len(words); i++ {
+		if words[i] == "(cap)" {
+			if i-1 >= 0 {
+				words[i-1] = capitalizeWord(words[i-1])
+			}
+			words = append(words[:i], words[i+1:]...)
+			i--
+			continue
+		}
 
-        if words[i] == "(cap," && i+1 < len(words) {
-            countStr := strings.TrimSuffix(words[i+1], ")")
-            num, err := strconv.Atoi(countStr)
-            if err == nil {
-                start := i - num
-                if start < 0 {
-                    start = 0
-                }
-                for j := start; j < i; j++ {
-                    words[j] = capitalizeWord(words[j])
-                }
-            }
-            words = append(words[:i], words[i+2:]...)
-            i--
-        }
-    }
+		if words[i] == "(cap," && i+1 < len(words) {
+			countStr := strings.TrimSuffix(words[i+1], ")")
+			num, err := strconv.Atoi(countStr)
+			if err == nil {
+				start := i - num
+				if start < 0 {
+					start = 0
+				}
+				for j := start; j < i; j++ {
+					words[j] = capitalizeWord(words[j])
+				}
+			}
+			words = append(words[:i], words[i+2:]...)
+			i--
+		}
+	}
 
-    return strings.Join(words, " ")
+	return strings.Join(words, " ")
 }
 
 func applyTransformation(s string) string {
 	s = converter(s)
-	s = fixArticle(s)
-	s = fixPunctuation(s)
 	s = toUpperCase(s)
 	s = toLowerCase(s)
 	s = toTitleCase(s)
 
 	return s
 }
+
+/*func readFile(filename string) string {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+
+	return string(data)
+}
+
+func writeFile(filename string, content string) {
+	err := os.WriteFile(filename, []byte(content+"\n"), 0644)
+	if err != nil {
+		fmt.Println("Error writing file:", err)
+	}
+}
+
+func main() {
+
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: go run . input.txt output.txt")
+	}
+
+	input1 := os.Args[1]
+	input2 := os.Args[2]
+
+	content := readFile(input1)
+	content = applyTransformation(content)
+	writeFile(input2, content)
+}*/
--- sentinel-reborn/main.go
+++ sentinel-reborn/main.go
@@ -3,51 +3,81 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
+	//"strings"
 )
 
-func LowerCase(text string) string {
-	words := strings.Fields(text)
-	for i, s := range words {
-		if s == "(low," {
-			n := int(words[i+1][0] - '0')
+/*
+	func LowerCase(text string) string {
+		words := strings.Fields(text)
+		for i, s := range words {
+			if s == "(low," {
+				n := int(words[i+1][0] - '0')
 
-			for j := i - n; j < i; j++ {
-				if j >= 0 {
-					words[j] = strings.ToLower(words[j])
+				for j := i - n; j < i; j++ {
+					if j >= 0 {
+						words[j] = strings.ToLower(words[j])
+					}
 				}
-			}
-			words = append(words[:i], words[i+2:]...)
-			break
-			
-		}
+				words = append(words[:i], words[i+2:]...)
+				break
 
-	}
-	return strings.Join(words, " ")
+			}
+
+		}
+		return strings.Join(words, " ")
 
 }
+*/
+/*func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: go run . input.txt output.txt")
+		return
+	}
+
+	inputFile := os.Args[1]
+	outputFile := os.Args[2]
+
+	data, err := os.ReadFile(inputFile)
+	if err != nil {
+		fmt.Println("Error reading sample file:", err)
+		return
+	}
+
+	result := applyTransformation(string(data))
+
+	err = os.WriteFile(outputFile, []byte(result), 0644)
+	if err != nil {
+		fmt.Println("Error writing result file:", err)
+		return
+	}
+}*/
+
+func readFile(filename string) string {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+
+	return string(data)
+}
+
+func writeFile(filename string, content string) {
+	err := os.WriteFile(filename, []byte(content+"\n"), 0644)
+	if err != nil {
+		fmt.Println("Error writing file:", err)
+	}
+}
+
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: go run main.go input.txt output.txt")
-		return
+
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: go run . input.txt output.txt")
 	}
 
-	inputfile := os.Args[1]
-	outputfile := os.Args[2]
+	input1 := os.Args[1]
+	input2 := os.Args[2]
 
-	data, err := os.ReadFile(inputfile)
-	if err != nil {
-		fmt.Println("error reading file:", err)
-		return
-	}
-
-	result := LowerCase(string(data))
-
-	err = os.WriteFile(outputfile, []byte(result), 0644)
-	if err != nil {
-		fmt.Println("error writing file:", err)
-		return
-	}
-
-	
+	content := readFile(input1)
+	content = applyTransformation(content)
+	writeFile(input2, content)
 }
